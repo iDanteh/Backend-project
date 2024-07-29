94b4e65783241b41001af466801842759f43ac48
iDanteh <105389532+iDanteh@users.noreply.github.com>
2024-07-29 16:35:48 -0600

    first commit

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Animal;
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Animal $animal)
+    {
+        //
+    }
+}
